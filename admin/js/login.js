17a8871c8ea0f6f951ca3c39388776eaf4baa39d
许言浩 <2677965586@qq.com>
2020-08-27 21:17:05 +0800

    登陆功能完成

---
+++ admin/js/login.js
@@ -0,0 +1,27 @@
+$(function() {
+    $('.input_sub').on('click', function(e) {
+        // 阻止按钮默认行为
+        e.preventDefault()
+        var username = $('.input_txt').val().trim()
+        var password = $('.input_pass').val().trim()
+        if (username == '' || password == '') {
+           alert('账号或密码不能为空')
+           return
+        }
+        $.ajax({
+            type: 'post',
+            url: 'http://localhost:8080/api/v1/admin/user/login',
+            data: $('#form').serialize(),
+            success: function(data) {
+               $('#myModal').modal('show')
+               $('.modal-body p').text(data.msg)
+               if (data.code === 200) {
+                   $('#myModal').on('hidden.bs.modal', function (e) {
+                     location.href = '../../admin/index.html'
+                   })
+                   
+               }
+            }
+        })        
+    })
+})
